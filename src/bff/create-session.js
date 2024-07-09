6fc7f96625503ef9049dfb12ab90f580a59c10f8
SergeyMankevich <this_mail_for_everything@mail.ru>
2024-07-09 20:39:31 +0300

    Создать BFF

---
+++ src/bff/create-session.js
@@ -0,0 +1,30 @@
+import { ROLE } from '../constants';
+import { removeComment } from './session';
+
+export const createSession = (roleId) => {
+	const session = {
+		logout() {
+			Object.keys(session).forEach((key) => {
+				delete session[key];
+			});
+		},
+	};
+
+	switch (roleId) {
+		case ROLE.ADMIN: {
+			session.removeComment = removeComment;
+			break;
+		}
+		case ROLE.MODERATOR: {
+			session.removeComment = removeComment;
+			break;
+		}
+		case ROLE.READER: {
+		}
+
+		default:
+			break;
+	}
+
+	return session;
+};
